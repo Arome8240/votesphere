a2f9d06dc87c29ec4cf6ea6ff88aba504efb4ccb
Arome8240 <arome8240@gmail.com>
2025-04-22 14:04:14 +0100

    dskdk dlfdl

--- packages/frontend/app/_layout.tsx
+++ packages/frontend/app/_layout.tsx
@@ -1,19 +1,66 @@
 //import "../index.js";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClusterProvider } from "@/utils/cluster-data-access";
 import { ConnectionProvider } from "@/utils/ConnectionProvider";
+import {
+  Ubuntu_300Light,
+  Ubuntu_300Light_Italic,
+  Ubuntu_400Regular,
+  Ubuntu_400Regular_Italic,
+  Ubuntu_500Medium,
+  Ubuntu_500Medium_Italic,
+  Ubuntu_700Bold,
+  Ubuntu_700Bold_Italic,
+  useFonts,
+} from "@expo-google-fonts/ubuntu";
+import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "react-native";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_300Light,
+    Ubuntu_300Light_Italic,
+    Ubuntu_400Regular,
+    Ubuntu_400Regular_Italic,
+    Ubuntu_500Medium,
+    Ubuntu_500Medium_Italic,
+    Ubuntu_700Bold,
+    Ubuntu_700Bold_Italic,
+  });
+  React.useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <QueryClientProvider client={queryClient}>
-      <ClusterProvider>
-        <ConnectionProvider config={{ commitment: "processed" }}>
-          <Stack />
-        </ConnectionProvider>
-      </ClusterProvider>
-    </QueryClientProvider>
+    <React.Fragment>
+      <StatusBar
+        barStyle="dark-content"
+        showHideTransition="slide"
+        translucent
+        backgroundColor={"transparent"}
+      />
+      <GluestackUIProvider mode="light">
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <QueryClientProvider client={queryClient}>
+            <ClusterProvider>
+              <ConnectionProvider config={{ commitment: "processed" }}>
+                <Stack />
+              </ConnectionProvider>
+            </ClusterProvider>
+          </QueryClientProvider>
+        </GestureHandlerRootView>
+      </GluestackUIProvider>
+    </React.Fragment>
   );
 }
