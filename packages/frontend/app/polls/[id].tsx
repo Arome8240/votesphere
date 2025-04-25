51b743f1a49f482bd5cdeb8f8b7356a6a4159baf
Arome8240 <arome8240@gmail.com>
2025-04-25 03:20:34 +0100

    Completed Votesphere app

--- packages/frontend/app/polls/[id].tsx
+++ packages/frontend/app/polls/[id].tsx
@@ -4,11 +4,30 @@ import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text } from "@/components/ui/text";
 import { usePoll } from "@/utils/useCreatePoll";
+import { useMobileWallet } from "@/utils/useMobileWallet";
+import { PlusCircle, XCircle } from "lucide-react-native";
+import {
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@/components/ui/modal";
+import { Button } from "@/components/ui/button";
+import { TextInput } from "react-native";
 
 export default function id() {
   const { id } = useLocalSearchParams();
   //console.log("Poll ID:", id); // Check this value
-  const { pollDetail } = usePoll(id as string);
+  const { pollDetail, candidates, registerCandidate, hasUserVoted, vote } =
+    usePoll(id as string);
+
+  const [showModal, setShowModal] = React.useState(false);
+  const [candidateName, setCandidateName] = React.useState("");
+
+  console.log(candidates.data);
   return (
     <SafeAreaView className="flex-1 p-5 bg-white">
       <Text className="" size="2xl">
@@ -43,6 +62,121 @@ export default function id() {
           </Pressable>
         )}
         {pollDetail.isError && <Text>{pollDetail.error.message}</Text>}
+      </View>
+      <View className="mt-5">
+        <View className="justify-between flex-row items-center">
+          <Text size="xl">Candidates</Text>
+
+          {pollDetail.data?.id != null && (
+            <Pressable
+              onPress={() => setShowModal(true)}
+              // onPress={() => {
+              //   registerCandidate.mutateAsync({
+              //     name: "Arome",
+              //     pollId: pollDetail.data!.id,
+              //   });
+              // }}
+            >
+              <PlusCircle size={20} color={"blue"} />
+            </Pressable>
+          )}
+        </View>
+        <View className="rounded-lg border border-gray-300 bg-gray-50 mt-3 p-5">
+          {candidates.isPending ? (
+            <Text>Fetching Candidates</Text>
+          ) : (
+            <View className="gap-y-4">
+              {candidates.data && candidates.data?.length > 0 ? (
+                candidates.data?.map((candidate, index) => (
+                  <View key={index} className="flex-row justify-between">
+                    <View>
+                      <Text>Name: {candidate.name}</Text>
+                      <Text>
+                        Has Registered:{" "}
+                        {candidate?.hasRegistered ? "Yes" : "No"}
+                      </Text>
+                    </View>
+
+                    <View>
+                      <Pressable
+                        android_ripple={{
+                          color: hasUserVoted.data
+                            ? "oklch(79.2% 0.209 151.711)"
+                            : "transparent",
+                        }}
+                        className={`${
+                          hasUserVoted.data ? "bg-red-700" : "bg-green-700"
+                        } rounded-lg p-2`}
+                        disabled={hasUserVoted.data}
+                        onPress={() => {
+                          vote.mutateAsync({
+                            pollId: pollDetail.data!.id,
+                            candidateId: candidate.cid,
+                          });
+                        }}
+                      >
+                        {hasUserVoted.data ? (
+                          <Text className="text-white" size="lg">
+                            Voted {candidate.votes}
+                          </Text>
+                        ) : (
+                          <Text className="text-white" size="lg">
+                            Vote {candidate.votes}
+                          </Text>
+                        )}
+                      </Pressable>
+                    </View>
+                  </View>
+                ))
+              ) : (
+                <Text size="lg">No Candidate Available on this Poll.</Text>
+              )}
+            </View>
+          )}
+        </View>
+
+        <Modal
+          isOpen={showModal}
+          onClose={() => {
+            setShowModal(false);
+          }}
+          size="md"
+        >
+          <ModalBackdrop />
+          <ModalContent>
+            <ModalHeader>
+              <Text size="md" className="text-typography-950">
+                Add Candidate
+              </Text>
+              <ModalCloseButton>
+                <XCircle color={"gray"} />
+              </ModalCloseButton>
+            </ModalHeader>
+            <ModalBody>
+              <TextInput
+                placeholder="Candidate Name"
+                value={candidateName}
+                onChangeText={setCandidateName}
+                className="p-4 border border-gray-200 mt-3 rounded-lg"
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                onPress={() => {
+                  if (candidateName) {
+                    registerCandidate.mutateAsync({
+                      name: candidateName,
+                      pollId: pollDetail.data!.id,
+                    });
+                    setShowModal(false);
+                  }
+                }}
+              >
+                {registerCandidate.isPending ? "Adding" : "Add Candidate"}
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </View>
     </SafeAreaView>
   );
--- packages/frontend/utils/useCreatePoll.ts
+++ packages/frontend/utils/useCreatePoll.ts
@@ -7,7 +7,7 @@ import { useAnchorWallet } from "./useAnchorWallet";
 import { useMemo } from "react";
 import * as anchor from "@coral-xyz/anchor";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { Poll } from "./interfaces";
+import { Candidate, Poll } from "./interfaces";
 
 // Create a poll
 // const createPoll = async (
@@ -56,6 +56,24 @@ export const usePoll = (pollAddress?: string) => {
     return PublicKey.findProgramAddressSync([counterSeed], pollProgramId);
   }, [pollProgramId]);
 
+  const [voterPDA] = useMemo(() => {
+    const voterSeed = anchor.utils.bytes.utf8.encode("voter");
+
+    return PublicKey.findProgramAddressSync([voterSeed], pollProgramId);
+  }, [pollProgramId]);
+
+  const [pollPDA] = useMemo(() => {
+    const pollSeed = anchor.utils.bytes.utf8.encode("poll");
+
+    return PublicKey.findProgramAddressSync([pollSeed], pollProgramId);
+  }, [pollProgramId]);
+
+  const [registerPDA] = useMemo(() => {
+    const registerSeed = anchor.utils.bytes.utf8.encode("registerations");
+
+    return PublicKey.findProgramAddressSync([registerSeed], pollProgramId);
+  }, [pollProgramId]);
+
   const provider = useMemo(() => {
     if (!anchorWallet) return;
     return new AnchorProvider(connection, anchorWallet, {
@@ -84,45 +102,6 @@ export const usePoll = (pollAddress?: string) => {
       return serializedPolls;
     },
   });
-
-  const pollDetail = useQuery({
-    queryKey: ["poll-detail", pollAddress],
-    enabled: !!program && !!pollAddress,
-    queryFn: async (): Promise<Poll | null> => {
-      if (!pollAddress) throw new Error("Poll address is required");
-      const address = new PublicKey(pollAddress);
-
-      //console.log(address, pollAddress);
-
-      const poll = await program?.account.poll.fetch(address);
-
-      if (!poll) throw new Error("Poll not found");
-
-      const serialized: Poll = {
-        ...poll,
-        publicKey: pollAddress,
-        id: poll.id.toNumber(),
-        start: poll.start.toNumber() * 1000,
-        end: poll.end.toNumber() * 1000,
-        candidates: poll.candidates.toNumber(),
-      };
-
-      return serialized;
-    },
-  });
-
-  const serializedPoll = (polls: any[]): Poll[] =>
-    polls.map((c: any) => {
-      //console.log("Raw Poll Data:", c.account);
-      return {
-        ...c.account,
-        publicKey: c.publicKey.toBase58(),
-        id: c.account.id.toNumber(),
-        start: c.account.start.toNumber() * 1000,
-        end: c.account.end.toNumber() * 1000,
-        candidates: c.account.candidates.toNumber(),
-      };
-    });
 
   const count = useQuery({
     queryKey: ["counter"],
@@ -208,12 +187,215 @@ export const usePoll = (pollAddress?: string) => {
       console.error("Poll Creation Error", error.message),
   });
 
+  const pollDetail = useQuery({
+    queryKey: ["poll-detail", pollAddress],
+    enabled: !!program && !!pollAddress,
+    queryFn: async (): Promise<Poll | null> => {
+      if (!pollAddress) throw new Error("Poll address is required");
+      const address = new PublicKey(pollAddress);
+
+      //console.log(address, pollAddress);
+
+      const poll = await program?.account.poll.fetch(address);
+
+      if (!poll) throw new Error("Poll not found");
+
+      const serialized: Poll = {
+        ...poll,
+        publicKey: pollAddress,
+        id: poll.id.toNumber(),
+        start: poll.start.toNumber() * 1000,
+        end: poll.end.toNumber() * 1000,
+        candidates: poll.candidates.toNumber(),
+      };
+
+      return serialized;
+    },
+  });
+
+  const candidates = useQuery({
+    queryKey: ["candidates", pollAddress],
+    enabled: !!program && !!pollAddress,
+    queryFn: async (): Promise<Candidate[]> => {
+      if (!pollAddress) throw new Error("Poll address is required");
+      const address = new PublicKey(pollAddress);
+
+      const pollData = await program?.account.poll.fetch(address);
+      if (!pollData) return [];
+
+      const PID = new BN(pollData.id);
+
+      const candidateAccounts = await program?.account.candidate.all();
+      const candidates = candidateAccounts
+        ?.filter((candidate) => candidate.account.pollId.eq(PID))
+        .map((candidate) => ({
+          cid: candidate.account.cid.toNumber(),
+          pollId: candidate.account.pollId.toNumber(),
+          publicKey: candidate.publicKey.toString(),
+          name: candidate.account.name, // Candidate name
+          hasRegistered: candidate.account.hasRegistered, // Whether they have registered
+          votes: candidate.account.votes.toNumber(), // Votes (assuming it's a BN that needs conversion)
+        }));
+
+      return candidates ?? [];
+    },
+  });
+
+  const hasUserVoted = useQuery({
+    queryKey: ["has-voted", pollAddress],
+    enabled: !!program && !!pollAddress,
+    queryFn: async (): Promise<boolean> => {
+      const voterAccount = await program?.account.voter.fetch(voterPDA);
+      if (!voterAccount || !voterAccount.hasVoted) {
+        return false; // Default value if no account exists or hasn't voted
+      }
+
+      return true;
+    },
+  });
+
+  const registerCandidate = useMutation({
+    mutationKey: ["candidates", pollAddress],
+    //enabled: !!program && !!pollAddress,
+    mutationFn: async ({ name, pollId }: { name: string; pollId: number }) => {
+      if (!program || !anchorWallet?.publicKey || !count.data?.count) {
+        console.log("program:", program);
+        console.log("wallet:", anchorWallet?.publicKey?.toBase58());
+        console.log("count data:", count.data);
+        throw new Error("Poll program not ready");
+      }
+
+      const PID = new BN(pollId as number);
+      const [pollPda] = PublicKey.findProgramAddressSync(
+        [PID.toArrayLike(Buffer, "le", 8)],
+        program.programId
+      );
+      const [registerationsPda] = PublicKey.findProgramAddressSync(
+        [Buffer.from("registerations")],
+        program.programId
+      );
+
+      const regs = await program.account.registerations.fetch(
+        registerationsPda
+      );
+      const CID = regs.count.add(new BN(1));
+
+      const [candidatePda] = PublicKey.findProgramAddressSync(
+        [PID.toArrayLike(Buffer, "le", 8), CID.toArrayLike(Buffer, "le", 8)],
+        program.programId
+      );
+
+      return await program.methods
+        .registerCandidate(PID, name)
+        .accountsPartial({
+          user: anchorWallet.publicKey,
+          poll: pollPda,
+          registerations: registerationsPda,
+          candidate: candidatePda,
+          systemProgram: SystemProgram.programId,
+        })
+        .rpc();
+    },
+    onSuccess: async (sig: string) => {
+      console.log("Candidate created with signature:", sig);
+      await candidates.refetch();
+    },
+
+    onError: (error: Error) =>
+      console.error("Poll Creation Error", error.message),
+  });
+
+  const vote = useMutation({
+    mutationKey: ["candidates", pollAddress],
+    //enabled: !!program && !!pollAddress,
+    mutationFn: async ({
+      candidateId,
+      pollId,
+    }: {
+      candidateId: number;
+      pollId: number;
+    }) => {
+      if (!program || !anchorWallet?.publicKey || !count.data?.count) {
+        console.log("program:", program);
+        console.log("wallet:", anchorWallet?.publicKey?.toBase58());
+        console.log("count data:", count.data);
+        throw new Error("Poll program not ready");
+      }
+
+      const PID = new BN(pollId);
+      const CID = new BN(candidateId);
+
+      const [pollPda] = PublicKey.findProgramAddressSync(
+        [PID.toArrayLike(Buffer, "le", 8)],
+        program.programId
+      );
+
+      const [voterPDA] = PublicKey.findProgramAddressSync(
+        [
+          Buffer.from("voter"),
+          PID.toArrayLike(Buffer, "le", 8),
+          anchorWallet.publicKey.toBuffer(),
+        ],
+        program.programId
+      );
+
+      const [candidatePda] = PublicKey.findProgramAddressSync(
+        [PID.toArrayLike(Buffer, "le", 8), CID.toArrayLike(Buffer, "le", 8)],
+        program.programId
+      );
+
+      return await program.methods
+        .vote(PID, CID)
+        .accountsPartial({
+          user: anchorWallet.publicKey,
+          poll: pollPda,
+          candidate: candidatePda,
+          voter: voterPDA,
+          systemProgram: SystemProgram.programId,
+        })
+        .rpc();
+    },
+    onSuccess: async (sig: string) => {
+      console.log("Vote add with signature:", sig);
+      await candidates.refetch();
+    },
+
+    onError: (error: Error) => console.error("Vote Error", error.message),
+  });
+
+  const serializedCandidates = (candidates: any[]): Candidate[] =>
+    candidates.map((c: any) => ({
+      ...c.account,
+      publicKey: c.publicKey.toBase58(), // Convert to string
+      cid: c.account.cid.toNumber(),
+      pollId: c.account.pollId.toNumber(),
+      votes: c.account.votes.toNumber(),
+      name: c.account.name,
+    }));
+
+  const serializedPoll = (polls: any[]): Poll[] =>
+    polls.map((c: any) => {
+      //console.log("Raw Poll Data:", c.account);
+      return {
+        ...c.account,
+        publicKey: c.publicKey.toBase58(),
+        id: c.account.id.toNumber(),
+        start: c.account.start.toNumber() * 1000,
+        end: c.account.end.toNumber() * 1000,
+        candidates: c.account.candidates.toNumber(),
+      };
+    });
+
   return {
     count,
     createPoll,
     polls,
     createCounter,
     pollDetail,
+    candidates,
+    hasUserVoted,
+    registerCandidate,
+    vote,
     // fetchPoll,
     // voteOnPoll,
     // closePoll,
